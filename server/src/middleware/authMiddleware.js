4d15de3a044c91bbdb3f0dc228140bbd28f80c52
Sailash05 <sailashs2005@gmail.com>
2025-07-06 15:04:08 +0530

    Backend basic authentication completed

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import { response } from '../utils/response.js';
+import jwt from "jsonwebtoken";
 
 const isValidEmail = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) && email === email.toLowerCase();
@@ -48,3 +49,22 @@ export const validateLoginInput = (req, res, next) => {
     
     next();
 }
+
+export const verifyToken = (req, res, next) => {
+    console.log('hdsf');
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+
+    const token = authHeader.split(" ")[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } 
+    catch (err) {
+        return res.status(403).json({ message: "Invalid or expired token." });
+    }
+};
